0e9b0ada82b7138e25b2cd7b1ee55b4880b570db
John Amakye <john.amakye@minneapolismn.gov>
2016-12-01 09:02:09 -0600

    api tutorial

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+
+console.log("javascript is here!!!");
+$(document).ready(function(){
+console.log("jquery is here");
+
+//begin
+$(document).on("click", "#clickMe",function(){
+ var movieTitle = $('#title').val();
+ console.log("Movie Title:", movieTitle);
+
+var movieURL= "http://www.omdbapi.com/?s=" +movieTitle;
+console.log("Movie URL: ", movieURL);
+$.ajax({
+  url: movieURL,
+  dataType: "JSON",
+  success: function(info){
+    console.log("ajax success data:", info.Search);
+    // startObject=info.Search);
+    displayOnDOM(info.Search);
+  },
+  statusCode:{
+    404: function(){
+      alert("error connecting to resources");
+    }
+  }
+});
+});
+});
+
+var displayOnDOM = function(display){
+  $("#showMe").empty();
+  for( var i = 0 ; i < display.length; i++ ){
+    $( "#showMe").append( "<p><b>" + display[ i ].Title + "</b> (" + display[ i ].Year + ")</p>");
+    $( "#showMe").append( "<img src=" + display[ i ].Poster +  ">");
+}
+};
